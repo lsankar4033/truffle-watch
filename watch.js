ffd0d872be416b0715f649b6ba42f9228cd5d83a
lsankar4033 <lsankar4033@gmail.com>
2019-08-04 10:58:29 -0700

    Proper queuing for a single command!

--- watch.js
+++ watch.js
@@ -1,14 +1,34 @@
 const chokidar = require('chokidar');
-const { spawn } = require('child_process');
+const Queue = require('better-queue');
+const { spawn, spawnSync } = require('child_process');
+
+// Object that keeps track of which names have active processes
+nameToActiveProcess = {}
+
+processQueue = new Queue((procDef, cb) => {
+  const [name, cmd, args] = procDef;
+
+  spawnSync(cmd, args, {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: [process.stdin, process.stdout, process.stderr],
+    encoding: 'utf-8'
+  })
+
+  nameToActiveProcess[name] = false;
+
+  cb(null);
+});
+
 
 function setupWatcher(name, regexes, cmd, args) {
   chokidar.watch(regexes).on('change', async (event, path) => {
-    const proc = spawn(cmd, args);
+    // NOTE: We might need to lock on nameToActiveProcess
+    if (!nameToActiveProcess[name]) {
+      nameToActiveProcess[name] = true;
 
-    proc.stdout.on('data', (data) => {
-      process.stdout.write(`${data}`);
-    });
-
+      processQueue.push([name, cmd, args]);
+    }
   });
 }
 
