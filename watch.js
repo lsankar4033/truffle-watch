794780da106d2d689d6240d30b49e19a2d98cdfd
lsankar4033 <lsankar4033@gmail.com>
2019-08-04 10:18:51 -0700

    Dead simple initial version of test command

--- watch.js
+++ watch.js
@@ -1,6 +1,19 @@
-/**
- * TODO
- */
+const chokidar = require('chokidar');
+const { spawn } = require('child_process');
+
+function setupWatcher(name, regexes, cmd, args) {
+  chokidar.watch(regexes).on('change', async (event, path) => {
+    const proc = spawn(cmd, args);
+
+    proc.stdout.on('data', (data) => {
+      process.stdout.write(`${data}`);
+    });
+
+  });
+}
+
 module.exports = async (config) => {
-  console.log('Hello world');
+
+  setupWatcher('test', ['contracts/', 'test/'], 'truffle', ['test']);
+
 }
